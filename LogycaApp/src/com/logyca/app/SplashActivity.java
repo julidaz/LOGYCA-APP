411b461c0f2a7ce3fe5f08d7fb11ecb3af41342c
Julian Daza <juliancamilo@platafor.ma>
2014-04-15 12:40:18 -0500

    No theme :), fonts and posible font load process.

--- LogycaApp/src/com/logyca/app/SplashActivity.java
+++ LogycaApp/src/com/logyca/app/SplashActivity.java
@@ -2,8 +2,10 @@ package com.logyca.app;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.TextView;
 
 public class SplashActivity extends Activity {
 
@@ -14,7 +16,12 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
- 
+        
+        //Custom font process
+        //TextView myTextView=(TextView)findViewById(id);
+        Typeface typeFace=Typeface.createFromAsset(getAssets(),"fonts/Museo-300.otf");
+        //myTextView.setTypeface(typeFace);
+        
         new Handler().postDelayed(new Runnable() {
  
             /*
@@ -26,8 +33,8 @@ public class SplashActivity extends Activity {
             public void run() {
                 // This method will be executed once the timer is over
                 // Start your app main activity
-                Intent i = new Intent(SplashActivity.this, /*Acá va la nueva actividad*/SplashActivity.class);
-                startActivity(i);
+                //Intent i = new Intent(SplashActivity.this, /*Acá va la nueva actividad*/SplashActivity.class);
+                //startActivity(i);
  
                 // close this activity
                 finish();
