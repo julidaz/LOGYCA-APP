a6c6a570b35a3b0307fe67a98a336baaf208e418
Julian Daza <juliancamilo@platafor.ma>
2014-05-26 14:24:28 -0500

    Register completed with all the validations :)

--- Logyca/src/com/logyca/RegisterActivity.java
+++ Logyca/src/com/logyca/RegisterActivity.java
@@ -1,20 +1,33 @@
 package com.logyca;
 
 import com.google.analytics.tracking.android.EasyTracker;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
 import android.app.Activity;
 import android.app.ActionBar;
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.os.Build;
 
 public class RegisterActivity extends Activity {
-
+	final Context reg_context = this;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -70,6 +83,103 @@ public class RegisterActivity extends Activity {
     public void onStart() {
       super.onStart();
       EasyTracker.getInstance(this).activityStart(this);  // Add this method.
+      
+      Button btn = (Button)findViewById(R.id.btn_registrar);
+      btn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				TextView name = (TextView)findViewById(R.id.reg_name);
+				boolean name_b = true;
+				if(name.getText().toString().isEmpty()){ 
+					createAlert("Por favor digite el nombre del usuario."); 
+					name_b = false;
+				}
+				TextView email = (TextView)findViewById(R.id.reg_mail);
+				boolean email_b = true;
+				if(email.getText().toString().contains("@") == false || email.getText().toString().isEmpty()){
+					createAlert("Por favor digite la direccion de correo o el campo de correo no es valido.");
+					email_b = false;
+				}
+				TextView password = (TextView)findViewById(R.id.reg_psw);
+				boolean password_b = true;
+				if(password.getText().toString().isEmpty()){
+					createAlert("Por favor digite la contraseña.");
+					password_b = false;
+				}
+				TextView password_conf = (TextView)findViewById(R.id.reg_psw_cnf);
+				boolean password_conf_b = true;
+				if(password_conf.getText().toString().isEmpty()){
+					createAlert("Por escriba la contraseña nuevamente.");
+					password_conf_b = false;
+				}else if(password.getText().toString().equals(password_conf.getText().toString()) == false){
+					createAlert("Por verifique las contraseñas.");
+					password_conf_b = false;
+				}
+				//TO DO REGISTER IF ALL IS TRUE
+				Log.i("Register", "Can register?");
+				if(name_b && 
+				   email_b && 
+				   password_b && 
+				   password_conf_b && 
+				   password.getText().toString().equals(password_conf.getText().toString()) ){
+					Log.i("Register", "Going to register :)");
+					AsyncHttpClient client = new AsyncHttpClient();
+					String URL_complete = "http://www.tecnoeficiencia.com/movil/service.registros.php";
+					RequestParams params = new RequestParams();
+					params.put("correo", email.getText().toString());
+					params.put("clave", password.getText().toString());
+					params.put("nombre", name.getText().toString());
+					
+					client.get(null, URL_complete, params, new AsyncHttpResponseHandler() {
+					    @Override
+					    public void onSuccess(String response) {
+					    	if (response.contains("resultadoLogin\":\"true\"")){
+					    		Log.i("HTTPGet",response);
+					    		createAlert("Usuario registrado correctamente, sera dirigido a la pantalla principal.");
+					    		Intent i = new Intent(RegisterActivity.this, MainActivity.class );
+					    		startActivity(i);
+							}else{
+								createAlert("El registro no pudo ser realizado, por favor intente nuevamente.");
+							}
+					    }
+					    //Alert for the request if it's false
+					    private void createAlert(String string) {
+							// TODO Auto-generated method stub
+							AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(reg_context);
+							alertDialogBuilder.setTitle("Alerta");
+							alertDialogBuilder.setMessage(string);
+							alertDialogBuilder.setCancelable(true);
+							alertDialogBuilder.setNeutralButton("Aceptar",
+							        new DialogInterface.OnClickListener() {
+							    		public void onClick(DialogInterface dialog, int id) {
+							    			dialog.cancel();
+							    		}
+									});
+							AlertDialog alert = alertDialogBuilder.create();
+							alert.show();
+						}
+					});
+				}
+			}
+			
+			//Alert for the request if it's false
+		    private void createAlert(String string) {
+				// TODO Auto-generated method stub
+				AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(reg_context);
+				alertDialogBuilder.setTitle("Alerta");
+				alertDialogBuilder.setMessage(string);
+				alertDialogBuilder.setCancelable(true);
+				alertDialogBuilder.setNeutralButton("Aceptar",
+				        new DialogInterface.OnClickListener() {
+				    		public void onClick(DialogInterface dialog, int id) {
+				    			dialog.cancel();
+				    		}
+						});
+				AlertDialog alert = alertDialogBuilder.create();
+				alert.show();
+			}
+		});
     }
 
     @Override
